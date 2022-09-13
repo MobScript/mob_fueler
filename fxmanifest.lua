abe656749d03853ee6bc191849c7502f12db128f
MobScripts <113401404+MobScripts@users.noreply.github.com>
2022-09-12 23:02:07 -0600

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'V3CT0RBUG'
+description 'Fueler System'
+version '1.0'
+
+server_script 'main.lua'
+
+fueler_updater 'yes'
+fueler_name '^2Mob Fueler System'
+fueler_github 'https://github.com/MobScript/mob_fueler'
+
